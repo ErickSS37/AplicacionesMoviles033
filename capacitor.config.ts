53ca100816e40f32ad78dbd22839fcaed61438be
Erick Soriano Santillán <erick.soriano.santillan@gmail.com>
2023-08-27 23:15:26 -0600

    Se Crea Aplicación Hola Mundo

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Hola Mundo AM033 Erick Soriano',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
